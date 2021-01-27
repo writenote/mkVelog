1d6d54415653560b72567040049e47622a612e86
HanSol <hansol1773@gmail.com>
2021-01-27 16:20:41 +0900

    firebase not connect.....

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/Main.css';
 import styled from '@emotion/styled';
+import { firestore } from '../firebase';
 
 const Container = styled.div``;
 
--- src/pages/auth/Signin.js
+++ src/pages/auth/Signin.js
@@ -1,13 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import '../../css/auth/auth.css';
 import { clear } from '../../assets';
 import { useHistory, Link } from 'react-router-dom';
+import { firestore } from '../../firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 const Container = styled.div``;
 
 function Signin() {
   const history = useHistory();
+
+  const [eamil, setEamil] = useState('');
+  const handleClick = () => {};
+  const handleChange = (e) => {
+    setEamil(e.target.value);
+  };
+
+  const actionCodeSettings = {
+    url: 'velogtest.firebaseapp.com',
+    handleCodeInApp: true,
+    iOS: {
+      bundleId: 'com.example.ios',
+    },
+    android: {
+      packageName: 'com.example.android',
+      installApp: true,
+      minimumVersion: '12',
+    },
+    dynamicLinkDomain: 'example.page.link',
+  };
+
+  function emailLinkSend(email, { actionCodeSettings }) {
+    firebase
+      .auth()
+      .sendSignInLinkToEmail(email, actionCodeSettings)
+      .then(() => {
+        window.localStorage.setItem('emailForSignIn', email);
+        console.log('test____ss');
+      })
+      .catch((error) => {
+        console.log(error.code);
+      });
+  }
+
+  function emailLinkComplete() {
+    if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+      let email = window.localStorage.getItem('emailForSignIn');
+      if (!email) {
+        email = window.prompt('Please provide your email for confirmation');
+      }
+      firebase
+        .auth()
+        .signInWithEmailLink(email, window.location.href)
+        .then((result) => {
+          // Clear email from storage.
+          window.localStorage.removeItem('emailForSignIn');
+        })
+        .catch((error) => {
+          console.log(error.code);
+        });
+    }
+  }
+
+  function signOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  }
+
   return (
     <Container>
       <div className='popup'>
@@ -25,8 +93,8 @@ function Signin() {
           <h2>로그인</h2>
           <h4>이메일로 로그인</h4>
           <form>
-            <input placeholder='이메일을 입력하세요.' />
-            <button>로그인</button>
+            <input value={eamil} onChange={handleChange} placeholder='이메일을 입력하세요.' />
+            <button onClick={handleClick}>로그인</button>
           </form>
         </div>
         <div>
